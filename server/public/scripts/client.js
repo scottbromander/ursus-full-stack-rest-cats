fedfe441961342abebf1f1492005fdb4e3520541
Scott Bromander <scottbromander@gmail.com>
2020-04-03 13:33:07 -0500

    takes data from the front end and sends it to the server

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,4 +2,32 @@ $(document).ready(init);
 
 function init() {
   console.log("JQ up and rollin");
+
+  $("#js-submit-cat").on("submit", submitCat);
+}
+
+function submitCat(event) {
+  event.preventDefault();
+
+  const catInput = $("#js-input-cat").val();
+
+  postCat(catInput);
+
+  clearInput();
+}
+
+function postCat(cat) {
+  const dataForServer = {
+    cat: cat,
+  };
+
+  $.ajax({
+    type: "POST",
+    url: "/cat",
+    data: dataForServer,
+  });
+}
+
+function clearInput() {
+  $("#js-input-cat").val("");
 }
