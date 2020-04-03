2f10a41a161fea3bc25160d95fadcbe834f676fb
Scott Bromander <scottbromander@gmail.com>
2020-04-03 13:35:52 -0500

    adds cat router to server

--- server/server.js
+++ server/server.js
@@ -3,9 +3,14 @@ const app = express();
 const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 5000;
 
+//ROUTES
+const catRouter = require("./routes/cat.router");
+
 app.use(express.static("server/public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+
+app.use("/cat", catRouter);
 
 app.listen(PORT, () => {
   console.log(`Listening on port: ${PORT}`);
